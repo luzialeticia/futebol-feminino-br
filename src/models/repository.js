6d1c49b948a08064545eb70cda818c0a41151503
Letícia Luzia da Silva Furtado <leticialuzia@MacBook-Pro-de-Leticia.local>
2020-12-07 13:36:47 -0300

    Criando conexão com banco de dados

---
+++ src/models/repository.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const DB_URL = 'mongodb://localhost:27017/brazilian-soccer-championship'
+
+const connect = () => {
+    mongoose.connect(DB_URL, { userNewUrlParser: true })
+    const connection = mongoose.connection
+
+    connection.on('error', () => console.error('Error on connection!'))
+    connection.once('open', () => console.log('Database connected!'))
+}
+
+module.exports = { connect }
