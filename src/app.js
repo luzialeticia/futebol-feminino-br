0b4badeda3494915fd9fbc5a760f84bf2e7ccf7c
Letícia Luzia da Silva Furtado <leticialuzia@MacBook-Pro-de-Leticia.local>
2020-12-07 13:39:28 -0300

    Conectando com banco de dados no app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const db = require('./models/repository')
+
+db.connect()
 
 app.use(cors())
 app.use(express.json())
