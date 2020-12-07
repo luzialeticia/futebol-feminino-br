eaa8c4845d285608ad09d99dc0578fd732060a91
Letícia Luzia da Silva Furtado <leticialuzia@MacBook-Pro-de-Leticia.local>
2020-12-07 13:40:06 -0300

    Trocnado userNewUrlParser por useNewUrlParser

--- src/models/repository.js
+++ src/models/repository.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const DB_URL = 'mongodb://localhost:27017/brazilian-soccer-championship'
 
 const connect = () => {
-    mongoose.connect(DB_URL, { userNewUrlParser: true })
+    mongoose.connect(DB_URL, { useNewUrlParser: true })
     const connection = mongoose.connection
 
     connection.on('error', () => console.error('Error on connection!'))
