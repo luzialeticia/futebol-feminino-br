25aa5f286c7f6615e53aec182d8d1a9831f92ddf
Letícia Luzia da Silva Furtado <leticialuzia@MacBook-Pro-de-Leticia.local>
2020-12-07 13:41:50 -0300

    Chamando index.js no app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const db = require('./models/repository')
+const index = require('./routes/index')
 
 db.connect()
 
 app.use(cors())
 app.use(express.json())
+app.use('/', cors(), index)
 
 module.exports = app
