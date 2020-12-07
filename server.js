82b903d800aa7ec362af156c2c461ea6483f5c86
Letícia Luzia da Silva Furtado <leticialuzia@MacBook-Pro-de-Leticia.local>
2020-12-07 13:07:34 -0300

    Configurando server.js e chamando a porta

---
+++ server.js
@@ -0,0 +1,7 @@
+//const app = require('./src/app')
+const app = require('./src/app')
+const PORT = 8080
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
+})
