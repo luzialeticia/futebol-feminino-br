8663d0e3aa9e7da8d605e5ec39b844f3a7a214e2
Letícia Luzia <leticialuzia@MacBook-Pro-de-Leticia.local>
2020-12-07 18:43:36 -0300

    Criando model dos times

--- src/models/Teams.js
+++ src/models/Teams.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+/* Nome: String
+Nome popular: String
+Sigla: String
+Localidade: String
+Técnico: String
+Elenco: Array
+Escudo: String */
+
 const team = new Schema({
     _id: {
         type: mongoose.Schema.Types.ObjectId,
         auto: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    popularName: {
+        type: String,
+        required: false
+    },
+    initials: {
+        type: String,
         required: true
     },
     
