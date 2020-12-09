38aa82e180ecb37e4c8d42a4b37402263c103aaa
Letícia Luzia <leticialuzia@MacBook-Pro-de-Leticia.local>
2020-12-09 03:30:10 -0300

    Importando o arquivos das rotas

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const app = express()
 const cors = require('cors')
 const db = require('./models/repository')
 const index = require('./routes/index')
+const teams = require('./routes/Teams')
 
 db.connect()
 
 app.use(cors())
 app.use(express.json())
 app.use('/', cors(), index)
+app.use('/team', teams)
 
 module.exports = app
