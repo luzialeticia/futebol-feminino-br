413287742aa75e056db65b68cf8963c6a2178e3e
Letícia Luzia <leticialuzia@icloud.com>
2020-12-10 19:17:52 -0300

    Criando rota GET para getById

--- src/routes/Teams.js
+++ src/routes/Teams.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 router.get('/', cors(), controller.getAll)
 router.get('/state/:state', cors(), controller.getAllByState)
+router.get('/:id', cors(), controller.getById)
 router.post('/add', cors(), controller.addTeam)
 
 module.exports = router
