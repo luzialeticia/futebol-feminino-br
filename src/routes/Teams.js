afe71464088e80c4f22d19bc0cd4114b8eadf74e
Letícia Luzia <leticialuzia@icloud.com>
2020-12-09 23:48:24 -0300

    Corringo os nomes das funções nas rotas

--- src/routes/Teams.js
+++ src/routes/Teams.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const controller = require('../controllers/Teams')
 const cors = require('cors')
 
-router.get('/', cors(), controller.getAllTeams)
+router.get('/', cors(), controller.getAll)
 router.post('/add', cors(), controller.addTeam)
 
 module.exports = router
