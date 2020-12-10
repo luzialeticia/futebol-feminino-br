91acdf7c9fcb2f1e7a54bf01c03cad28533cd65f
Letícia Luzia <leticialuzia@icloud.com>
2020-12-09 23:12:30 -0300

    Importando src/database/db

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const db = require('./models/repository')
+const db = require('./database/db')
 const index = require('./routes/index')
 const teams = require('./routes/Teams')
 
