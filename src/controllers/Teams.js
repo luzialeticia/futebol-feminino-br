0589f5e2a3faacb167e729846afad6e56872a1b0
Letícia Luzia <leticialuzia@icloud.com>
2020-12-10 20:56:09 -0300

    Corrigindo funções getByState e getByPopularName

--- src/controllers/Teams.js
+++ src/controllers/Teams.js
@@ -7,40 +7,53 @@ module.exports = {
             if(err) {
                 return res.status(500).json('Erro ao buscar os times.')
             } else {
-                return res.status(200).send(teams)
+                return res.status(200).json(teams)
             }
         })
     },
 
+    //getAllByState
     getAllByState: (req, res) => {
         const state = req.params.state
-        //const capitalizeState = state.toUppercase()
+        const capitalizeState = state.toUpperCase()
 
-        Team.find({ state: state }, (err, teams) => {
+        Team.find({ state: capitalizeState }, (err, teams) => {
             if(err) {
-                return res.status(500).send('Erro ao buscar times.')
+                return res.status(500).json('Erro ao buscar times.')
             } else if(teams == "") {
                 return res.status(404).json('Times não encontrados.')
             } else {
-                res.status(200).send(teams)
+                res.status(200).json(teams)
             }
         })
     },
 
     //getById
     getById: (req, res) => {
-        const { id } = req.params
+        const id = req.params.id
 
-        Team.findById(id, (err, team) => {
+        Team.findById({ _id: id }, (err, team) => {
             if(err) {
-                res.status(400).send('Time não encontrado.')
-            } else  {
+                res.status(400).json('Time não encontrado.')
+            } else {
                 res.status(200).json(team)
             }
         })
     },
 
-    //getTeamByName
+    //getByPopularName
+    getByPopularName: (req, res) => {
+        const { name } = req.params
+        const capitalizeName = name.charAt(0).toUpperCase() + name.slice(1)
+
+        Team.find({ popularName: capitalizeName }, (err, team) => {
+            if(err) {
+                return res.status(500).json('Erro ao buscar time.')
+            } else {
+                res.status(200).json(team)
+            }
+        })
+    },
 
     //getTeamByInitials
 
@@ -53,7 +66,7 @@ module.exports = {
             if(err) {
                 return res.status(400).json(err)
             } else {
-                return res.status(202).send({
+                return res.status(202).json({
                     message: "Time cadastrado.",
                     team
                 })
--- src/routes/Teams.js
+++ src/routes/Teams.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 router.get('/', cors(), controller.getAll)
 router.get('/state/:state', cors(), controller.getAllByState)
 router.get('/:id', cors(), controller.getById)
+router.get('/name/:name', cors(), controller.getByPopularName)
 router.post('/add', cors(), controller.addTeam)
 
 module.exports = router
