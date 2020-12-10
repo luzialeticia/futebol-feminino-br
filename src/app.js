a9eb836bf52c381b90feca66f3862aca25cdb140
Letícia Luzia <leticialuzia@icloud.com>
2020-12-09 22:17:22 -0300

    Importando body-parser no app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 const cors = require('cors')
+const app = express()
 const db = require('./models/repository')
 const index = require('./routes/index')
 const teams = require('./routes/Teams')
 
 db.connect()
 
+app.use(bodyParser.json())
 app.use(cors())
-app.use(express.json())
 app.use('/', cors(), index)
 app.use('/teams', teams)
 
