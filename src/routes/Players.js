223da36cc2df5d65e76b5d4d68acfc8fe9149321
Letícia Luzia <leticialuzia@icloud.com>
2020-12-10 23:08:13 -0300

    Criando rota para getlAllByTeam

---
+++ src/routes/Players.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/Players')
+const cors = require('cors')
+
+router.get('/team/:team', cors(), controller.getAllByTeam)
