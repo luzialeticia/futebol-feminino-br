5085be92ec31ce934c4971dad5bf5e3d693cb392
Letícia Luzia <leticialuzia@icloud.com>
2020-12-11 01:48:36 -0300

    Reorganizando os códigos das rotas

--- src/routes/Teams.js
+++ src/routes/Teams.js
@@ -1,16 +1,19 @@
 const express = require('express')
-const router = express.Router()
+const Team = express.Router()
 const controller = require('../controllers/Teams')
 const cors = require('cors')
 
-router.get('/', cors(), controller.getAll)
-router.get('/state/:state', cors(), controller.getAllByState)
-router.get('/:id', cors(), controller.getById)
-router.get('/name/:name', cors(), controller.getByName)
-router.get('/initials/:initials', cors(), controller.getByInitials)
-router.post('/add', cors(), controller.addTeam)
-router.put('/update/:id', cors(), controller.update)
-router.patch('/update/:id', cors(), controller.update)
-router.delete('/delete/:id', cors(), controller.delete)
+Team.route('/', cors())
+    .get(controller.getAll)
+    .post(controller.addTeam)
 
-module.exports = router
+Team.route('/:id', cors())
+    .get(controller.getById)
+    .put(controller.update)
+    .patch(controller.update)
+
+Team.get('/sate/:state', cors(), controller.getAllByState)
+Team.get('/name/:name', cors(), controller.getByName)
+Team.get('/initials/:initials', cors(), controller.getByInitials)
+
+module.exports = Team
