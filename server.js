e67ea56106f7b6e9c60ee15439cac9913fd03886
Letícia Luzia <leticialuzia@icloud.com>
2020-12-11 13:34:30 -0300

    Configurando server.js com a porta do env

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./src/app')
-const PORT = 8080
+const dotEnv = require('dotenv')
+
+dotEnv.config()
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
