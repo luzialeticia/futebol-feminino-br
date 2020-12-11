9c0625fa31609d7ac4c9965181ab7bf0d9b4de17
Letícia Luzia <leticialuzia@icloud.com>
2020-12-11 14:47:25 -0300

    Renomeando título da API no index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     res.status(200).send({
-        titulo: "Campeonato Brasileiro de Futebol Feminino - Série A1",
+        titulo: "Futebol Feminino Brasileiro",
         edicao: "2020"
     })
 })
