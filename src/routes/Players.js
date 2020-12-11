a96de562738da30606223570125c26703b10d1a8
Letícia Luzia <leticialuzia@icloud.com>
2020-12-11 00:38:36 -0300

    Criando rotas para put e patch

--- src/routes/Players.js
+++ src/routes/Players.js
@@ -7,5 +7,7 @@ router.get('/team/:team', cors(), controller.getAllByTeam)
 router.get('/:id', cors(), controller.getById)
 router.get('/name/:name', controller.getByName)
 router.post('/add', cors(), controller.add)
+router.put('/update/:id', cors(), controller.update)
+router.patch('/update/:id', cors(), controller.update)
 
 module.exports = router
